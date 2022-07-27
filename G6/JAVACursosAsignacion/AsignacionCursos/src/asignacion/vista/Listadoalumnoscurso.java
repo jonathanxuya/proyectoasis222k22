97a19ef2a67dbdd0c12e0ee8ff8558c8a411db9e
joselyneR <linriveracifu@hotmail.com>
2022-07-26 20:13:31 -0600

    actualizaciondelistadocursosycertificacionJR

--- G6/JAVACursosAsignacion/AsignacionCursos/src/asignacion/vista/Listadoalumnoscurso.java
+++ G6/JAVACursosAsignacion/AsignacionCursos/src/asignacion/vista/Listadoalumnoscurso.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Joselyne Aracely Rivera Cifuentes
+/* carnet 0901-17-5
+/* Listado de alumnos por curso
  */
 package asignacion.vista;
 
